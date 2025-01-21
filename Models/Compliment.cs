4240e695ba37bff3cb2075c07ad0138ed9348978
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-21 12:04:07 +0000

    Add Compliments

--- Models/Compliment.cs
+++ Models/Compliment.cs
@@ -5,6 +5,27 @@
 		public int Id { get; set; }
 
 		public string Text { get; set; }
+		public static List<string> complimentStrings = new List<string>()
+		{
+			"You may not be evergreen, but you are to me.",
+			"I bet squirrels consider you prime real estate.",
+			"You're so down to earth.",
+			"You're very grounded.",
+			"Your leaves are soothing.",
+			"You sway nicely in the wind.",
+			"You're blooming brilliant.,ill and doing nothing.",
+			"Let's get to the root of your beauty.",
+			"I will never leaf you.",
+			"You've really branched out lately.",
+			"You provide the perfect amount of shade on a hot day.",
+			"I'm really pine-ing for you."
+		};
+		public static Compliment GetCompliment()
+		{
+			Random random = new Random();
+			int newId = random.Next(0, 12);
+			return new Compliment { Id = newId, Text = complimentStrings[newId] };
+		}
 
 	}
 }
--- Models/Tree.cs
+++ Models/Tree.cs
@@ -2,11 +2,20 @@
 {
 	public class Tree
 	{
+		public Tree(int id, string name, string latitude, string longitude, int size, string species)
+		{
+			Id = id;
+			Name = name;
+			Latitude = latitude;
+			Longitude = longitude;
+			Size = size;
+			Species = species;
+		}
 		public int Id { get; set; }
 
 		public string Name { get; set; }	
 
-		public string Latitude { get; set; }
+		public string Latitude { get; set; } 
 
 		public string Longitude { get; set; }
 
@@ -14,7 +23,7 @@
 
 		public string Species { get; set; }
 
-		public List<Compliment> Compliments { get; set; }
+		public List<Compliment> Compliments { get; set; } = new List<Compliment>();
 
 		
 	}
